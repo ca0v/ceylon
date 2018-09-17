8977fbd9a49dae73555f4a747c96e1033f68fe4d
calix <Corey.Alix@infor.com>
2018-09-17 07:54:37 -0400

    allow expect to remain the default

--- index.ts
+++ index.ts
@@ -5,3 +5,4 @@ import AssertionError, { IAssertionErrorOptions } from "./src/assertion-error";
 import Expectation from "./src/expectation";
 
 export { assert, expect, Expectation, IAssertOptions, AssertionError, IAssertionErrorOptions, deepEqual };
+export default expect;
