41e00b2c992a181c709bc01394b70bc763f5f1bf
calix <Corey.Alix@infor.com>
2018-09-18 14:36:56 -0400

    rrc -> ceylon

--- index.ts
+++ index.ts
@@ -1,8 +1,8 @@
-import expect from "./src/index";
-import assert, { IAssertOptions } from "./src/assert";
-import { equal as deepEqual } from "./src/fast-deep-equal";
-import AssertionError, { IAssertionErrorOptions } from "./src/assertion-error";
-import Expectation from "./src/expectation";
+import expect from "./ceylon/index";
+import assert, { IAssertOptions } from "./ceylon/assert";
+import { equal as deepEqual } from "./ceylon/fast-deep-equal";
+import AssertionError, { IAssertionErrorOptions } from "./ceylon/assertion-error";
+import Expectation from "./ceylon/expectation";
 
 export { assert, expect, Expectation, IAssertOptions, AssertionError, IAssertionErrorOptions, deepEqual };
 export default expect;
